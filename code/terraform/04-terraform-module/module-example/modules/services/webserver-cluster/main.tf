599bd7e2ffba12b57d1947181831859aac4c9b6e
Philip King <king.phil@gmail.com>
2020-05-29 18:00:08 -0500

    ch 4: copying over work from ch 2

--- code/terraform/04-terraform-module/module-example/modules/services/webserver-cluster/main.tf
+++ code/terraform/04-terraform-module/module-example/modules/services/webserver-cluster/main.tf
@@ -2,164 +2,115 @@ terraform {
   required_version = ">= 0.12, < 0.13"
 }
 
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0c55b159cbfafe1f0"
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.instance.id]
-  user_data       = data.template_file.user_data.rendered
+provider "kubernetes" {}
 
-  # Required when using a launch configuration with an auto scaling group.
-  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
-  lifecycle {
-    create_before_destroy = true
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx"
+    labels = {
+      App = "nginx"
+    }
   }
-}
 
-data "template_file" "user_data" {
-  template = file("${path.module}/user-data.sh")
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "nginx"
+      }
+    }
 
-  vars = {
-    server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
-  }
-}
+    template {
+      metadata {
+        labels = {
+          App = "nginx"
+        }
+      }
 
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
-  target_group_arns    = [aws_lb_target_group.asg.arn]
-  health_check_type    = "ELB"
+      spec {
+        container {
+          image = "nginx:1.18.0"
+          name  = "example"
 
-  min_size = var.min_size
-  max_size = var.max_size
+          port {
+            container_port = 80
+          }
 
-  tag {
-    key                 = "Name"
-    value               = var.cluster_name
-    propagate_at_launch = true
-  }
-}
-
-resource "aws_security_group" "instance" {
-  name = "${var.cluster_name}-instance"
-}
-
-resource "aws_security_group_rule" "allow_server_http_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.instance.id
-
-  from_port   = var.server_port
-  to_port     = var.server_port
-  protocol    = local.tcp_protocol
-  cidr_blocks = local.all_ips
-}
-
-resource "aws_lb" "example" {
-  name               = var.cluster_name
-  load_balancer_type = "application"
-  subnets            = data.aws_subnet_ids.default.ids
-  security_groups    = [aws_security_group.alb.id]
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-
-  port              = local.http_port
-
-  protocol          = "HTTP"
-
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
+          ## somewhat equivalent to AMI flavor
+          resources {
+            limits {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            # requests {
+            #   cpu    = "250m"
+            #   memory = "50Mi"
+            # }
+          }
+        }
+      }
     }
   }
 }
 
-resource "aws_lb_target_group" "asg" {
-  name     = var.cluster_name
-  port     = var.server_port
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 80
+      #node_port = 32001
+    }
 
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
+    type = "NodePort"
   }
 }
 
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
+## stolen from https://github.com/terraform-providers/terraform-provider-kubernetes/blob/master/_examples/ingress/main.tf
+resource "kubernetes_ingress" "nginx" {
+  metadata {
+    name = "nginx"
 
-  condition {
-    field  = "path-pattern"
-    values = ["*"]
+    annotations = {
+      ## pek note: see URL above; the example is wrong wrong wrong with its annotation
+      #"ingress.kubernetes.io/rewrite-target" = "/"
+      "nginx.ingress.kubernetes.io/rewrite-target" = "/"
+    }
   }
 
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-  }
-}
+  spec {
+    ## pek: not sure what this does, and it doesn't seem to be needed
+    # backend {
+    #   service_name = "nginx"
+    #   service_port = 80
+    # }
 
-resource "aws_security_group" "alb" {
-  name = "${var.cluster_name}-alb"
-}
+    rule {
+      host = "works.pekware.net"
 
-resource "aws_security_group_rule" "allow_http_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.alb.id
+      http {
+        path {
+          path = "/path-example"
 
-  from_port   = local.http_port
-  to_port     = local.http_port
-  protocol    = local.tcp_protocol
-  cidr_blocks = local.all_ips
-}
-
-resource "aws_security_group_rule" "allow_all_outbound" {
-  type              = "egress"
-  security_group_id = aws_security_group.alb.id
-
-  from_port   = local.any_port
-  to_port     = local.any_port
-  protocol    = local.any_protocol
-  cidr_blocks = local.all_ips
-}
-
-data "terraform_remote_state" "db" {
-  backend = "s3"
-
-  config = {
-    bucket = var.db_remote_state_bucket
-    key    = var.db_remote_state_key
-    region = "us-east-2"
+          backend {
+            service_name = "nginx"
+            service_port = 80
+          }
+        }
+      }
+    }
   }
 }
 
-locals {
-  http_port    = 80
-  any_port     = 0
-  any_protocol = "-1"
-  tcp_protocol = "tcp"
-  all_ips      = ["0.0.0.0/0"]
+output "node_port" {
+  value = kubernetes_service.nginx.spec[0].port[0].node_port
 }
 
-data "aws_vpc" "default" {
-  default = true
+output "URL" {
+  value = "${kubernetes_ingress.nginx.spec[0].rule[0].host}${kubernetes_ingress.nginx.spec[0].rule[0].http[0].path[0].path}"
 }
-
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
