10043ae7ffd4916a062f3127732436bc73bf2f7d
Philip King <king.phil@gmail.com>
2020-05-29 14:23:04 -0500

    nginx w/ingress! (working path example)

--- code/terraform/02-intro-to-terraform-syntax/02.one-webserver/main.tf
+++ code/terraform/02-intro-to-terraform-syntax/02.one-webserver/main.tf
@@ -2,48 +2,115 @@ terraform {
   required_version = ">= 0.12, < 0.13"
 }
 
-provider "aws" {
-  region = "us-east-2"
+provider "kubernetes" {}
 
-  # Allow any 2.x version of the AWS provider
-  version = "~> 2.0"
-}
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx"
+    labels = {
+      App = "nginx"
+    }
+  }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "nginx"
+      }
+    }
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+    template {
+      metadata {
+        labels = {
+          App = "nginx"
+        }
+      }
 
-  tags = {
-    Name = "terraform-example"
+      spec {
+        container {
+          image = "nginx:1.18.0"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          ## somewhat equivalent to AMI flavor
+          resources {
+            limits {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            # requests {
+            #   cpu    = "250m"
+            #   memory = "50Mi"
+            # }
+          }
+        }
+      }
+    }
   }
 }
 
-resource "aws_security_group" "instance" {
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.metadata[0].labels.App
+    }
+    port {
+      port        = 80
+      target_port = 80
+      #node_port = 32001
+    }
 
-  name = var.security_group_name
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    type = "NodePort"
   }
 }
 
-variable "security_group_name" {
-  description = "The name of the security group"
-  type        = string
-  default     = "terraform-example-instance"
+## stolen from https://github.com/terraform-providers/terraform-provider-kubernetes/blob/master/_examples/ingress/main.tf
+resource "kubernetes_ingress" "nginx" {
+  metadata {
+    name = "nginx"
+
+    annotations = {
+      ## pek note: see URL above; the example is wrong wrong wrong with its annotation
+      #"ingress.kubernetes.io/rewrite-target" = "/"
+      "nginx.ingress.kubernetes.io/rewrite-target" = "/"
+    }
+  }
+
+  spec {
+    ## pek: not sure what this does, and it doesn't seem to be needed
+    # backend {
+    #   service_name = "nginx"
+    #   service_port = 80
+    # }
+
+    rule {
+      host = "works.pekware.net"
+
+      http {
+        path {
+          path = "/path-example"
+
+          backend {
+            service_name = "nginx"
+            service_port = 80
+          }
+        }
+      }
+    }
+  }
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP of the Instance"
+output "node_port" {
+  value = kubernetes_service.nginx.spec[0].port[0].node_port
+}
+
+output "URL" {
+  value = "${kubernetes_ingress.nginx.spec[0].rule[0].host}${kubernetes_ingress.nginx.spec[0].rule[0].http[0].path[0].path}"
 }
